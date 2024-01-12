6c644ce4cb755d38cc56c2ca526db0abf387226f
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-01-12 23:47:02 +0600

    checking the reason of vercel error

--- src/controllers/teacherController.js
+++ src/controllers/teacherController.js
@@ -5,14 +5,17 @@
  * Date: 11/01/24
  */
 
+const HomeInfo = require("../models/homeSchema")
 const { successResponse } = require("./responseController")
 
 const handleGetTeacher = async (req, res, next) => {
     try {
+
+        const home = await HomeInfo.find({});
         successResponse(res, {
             statusCode: 200,
             message: "Teacher get successfully",
-            payload: {}
+            payload: {...home }
         })
     } catch (error) {
         
