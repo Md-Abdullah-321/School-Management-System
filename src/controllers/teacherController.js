34af5eed76133007a17149a053283d7ecebf4e80
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-01-12 23:50:48 +0600

    checking the reason of vercel error

--- src/controllers/teacherController.js
+++ src/controllers/teacherController.js
@@ -5,17 +5,15 @@
  * Date: 11/01/24
  */
 
-const HomeInfo = require("../models/homeSchema")
 const { successResponse } = require("./responseController")
 
 const handleGetTeacher = async (req, res, next) => {
     try {
-
-        const home = await HomeInfo.find({});
+        
         successResponse(res, {
             statusCode: 200,
             message: "Teacher get successfully",
-            payload: {...home }
+            payload: {}
         })
     } catch (error) {
         
--- src/models/homeSchema.js
+++ src/models/homeSchema.js
@@ -1,30 +1,16 @@
-/*
- * Title: Site Settings Schema. 
- * Description: handle Schema for site settings.
- * Author: Md Abdullah
- * Date: 12/01/24
- */
+// Title: Site Settings Schema.
+// Description: Handle Schema for site settings.
+// Author: Md Abdullah
+// Date: 12/01/24
 
-
-
-const { Schema, model } = require("mongoose");
-
+const { Schema, model } = require('mongoose');
 
 const homeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    logo: {
-        type: String,
-        required: true
-    },
-    backgroundImage: {
-        type: String,
-        required: true
-    }
+  name: { type: String, required: true },
+  logo: { type: String, required: true },
+  backgroundImage: { type: String, required: true },
 }, { timestamps: true });
 
-
 const HomeInfo = model('HomeInfo', homeSchema);
+
 module.exports = HomeInfo;
