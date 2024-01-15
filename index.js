0a8b2f69f0f58605167be668e301c56460fd2a44
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-01-15 21:47:37 +0600

    serve the client form one step back

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const app = require("./src/app");
+const path = require('path');
 const connectionDB = require("./src/config/db");
 
+//serve Client:
+app.use(express.static(path.join(__dirname,"./client/dist")));
 
+app.get("*", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, './client/dist/index.html'),
+        function(err) {
+            res.status(500).send(err)
+        }
+    )
+})
 
 app.listen(3000, async () => {
     console.log("Server Connected");
